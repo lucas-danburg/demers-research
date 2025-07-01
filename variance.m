aa192d0e00e055e677a907bc039fa63e0b8266f4
wleland2022 <william.leland@student.fairfield.edu>
2025-07-01 11:31:16 -0400

    faster

--- variance.m
+++ variance.m
@@ -144,14 +144,19 @@ function density = D(T, table)
     % this is in case of tables where t is not an arc length parameter
     [n, m] = size(T);
     density = zeros(n, m);
+    x = cell(size(table, 1), 1);
+    y = cell(size(table, 1), 1);
+    for p = 1:n_pieces
+        syms t;
+        x{p} = inline(char(diff(eval(char(table{p, 1})), t)));   %x'(t) for piece
+        y{p} = inline(char(diff(eval(char(table{p, 2})), t)));   %y'(t) for piece
+    end
+
     for i = 1:n
         for j = 1:m
             tval = T(i, j);
             p = piece(tval);
-            syms t;
-            x=inline(char(diff(eval(char(table{p, 1})),t)));   %x'(t) for piece
-            y=inline(char(diff(eval(char(table{p, 2})),t)));   %y'(t) for piece
-            density(i, j) = sqrt(x(tval)^2 + y(tval)^2);
+            density(i, j) = sqrt(x{p}(tval)^2 + y{p}(tval)^2);
         end
     end
     %disp('did density')
